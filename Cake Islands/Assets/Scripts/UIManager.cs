2a65bf37435a130c1d52ded72ab3d3d373cd3020
maddogmitch <mightymitch66@gmail.com>
2020-05-02 19:32:53 -0400

    added in main menu

---
+++ Cake Islands/Assets/Scripts/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //Turns it into an instance
+    private static UIManager instance = null;
+    public static UIManager inst { get { return instance; } }
+
+    public GameObject SettingsPanel;
+    public GameObject CreditsPanel;
+    public GameObject StartPanel;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        instance = null;
+    }
+
+    public void LoadPanel(GameObject panel)
+    {
+        ClosePanel();
+        panel.SetActive(true);
+        StartPanel.SetActive(false);
+    }
+
+    public void ClosePanel()
+    {
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        StartPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
