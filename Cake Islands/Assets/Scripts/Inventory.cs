5d88737a071a70ccb33d23aa59290d12d0ac2b00
maddogmitch <mightymitch66@gmail.com>
2020-05-01 22:53:49 -0400

    Moving Platforms, first door script, and folders

---
+++ Cake Islands/Assets/Scripts/Inventory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject[] inventory = new GameObject[5];
+
+    public void AddItem(GameObject item)
+    {
+
+        bool itemAdded = false;
+    
+        //find first open slot in inventory
+        for(int i = 0; i < inventory.Length; i++)
+        {
+            if(inventory[i] == null)
+            {
+                inventory[i] = item;
+                Debug.Log(item.name + " was added");
+                itemAdded = true;
+                //Do something with the object
+                item.SendMessage("DoInteraction");
+                break;
+            }
+        }
+
+        //inventory full
+        if(!itemAdded)
+        {
+            Debug.Log("Inventory is Full - Item not added");
+        }
+    }
+
+    public bool FindItem(GameObject item)
+    {
+        for(int i = 0; i < inventory.Length; i++)
+        {
+            if(inventory[i] == item)
+            {
+                //found the item
+                return true;
+            }
+        }
+        //item was not found
+        return false;
+    }
+}
---
+++ Cake Islands/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+    public Interaction currentInterObjScript = null;
+    public Inventory inventory;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown ("Interact") && currentInterObj)
+        {
+            //Check to see if this object is to be stored in inventory
+            if(currentInterObjScript.inventory)
+            {
+                inventory.AddItem(currentInterObj);
+            }
+
+            //Check to see if this object can be opened
+            if(currentInterObjScript.openable)
+            {
+                //Check if locked
+                if(currentInterObjScript.locked)
+                {
+                    //Check to see if we have the object to unlock
+                    //Search our inventory for the item needed - if found unlock object
+                    if(inventory.FindItem(currentInterObjScript.key))
+                    {
+                        //Found item needed unlock the door
+                        currentInterObjScript.locked = false;
+                        Debug.Log(currentInterObj.name + " was unlocked");
+                        currentInterObjScript.Open();
+                    }
+                    else
+                    {
+                        Debug.Log(currentInterObj.name + " was not unlocked");
+
+                    }
+                }
+                else
+                {
+                    //Door is not locked - open the door
+                    Debug.Log(currentInterObj.name + " is unlocked");
+                }
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("interactable"))
+        {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+            currentInterObjScript = currentInterObj.GetComponent<Interaction>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("interactable"))
+        {
+            if(other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+            
+        }
+
+    }
+
+}
