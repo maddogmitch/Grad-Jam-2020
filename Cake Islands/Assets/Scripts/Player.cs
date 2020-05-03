cb3d3c4dc4af403b50c8cf5cf5b3c883794697c0
maddogmitch <mightymitch66@gmail.com>
2020-05-03 12:26:56 -0400

    Added Beggening for Death

--- Cake Islands/Assets/Scripts/Player.cs
+++ Cake Islands/Assets/Scripts/Player.cs
@@ -103,7 +103,14 @@ public class Player : MonoBehaviour
         }
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("spikes"))
+        {
+           
+        }
+    }
+
 
     void FixedUpdate()
     {
